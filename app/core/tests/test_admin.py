3247939b4669be9b385b462374dcbc11a4869c52
annalitvin <litvin_any@ukr.net>
2019-03-19 21:48:58 +0200

    Update admin to support custom user model.

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -3,7 +3,6 @@ from django.contrib.auth import get_user_model
 from django.urls import reverse
 
 
-
 class AdminSiteTests(TestCase):
 
     def setUp(self):
@@ -26,3 +25,17 @@ class AdminSiteTests(TestCase):
 
         self.assertContains(res, self.user.name)
         self.assertContains(res, self.user.email)
+
+    def test_user_change_page(self):
+        """Test that the user edit page works"""
+        url = reverse('admin:core_user_change', args=[self.user.id])
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
+
+    def test_create_user_page(self):
+        """Test that the create user page works"""
+        url = reverse('admin:core_user_add')
+        res = self.client.get(url)
+
+        self.assertEqual(res.status_code, 200)
